083f000ca804b3020deb05192b911ae0124af881
Apoorva Koche <apoorvakoche57@gmail.com>
2026-08-24 14:31:23 +0530

    Time: 1 ms (97.53%) | Memory: 46.8 MB (99.15%) - LeetSync

--- 724-find-pivot-index/find-pivot-index.java
+++ 724-find-pivot-index/find-pivot-index.java
@@ -1,15 +1,13 @@
 class Solution {
     public int pivotIndex(int[] nums) {
-
+        int n=nums.length;
+        int arr[] = new int[nums.length+1];
+        Arrays.fill(arr,0);
+        for(int i=1;i<=nums.length;i++){
+            arr[i]=arr[i-1]+nums[i-1];
+        }
         for(int i=0;i<nums.length;i++){
-            int ls=0,rs=0;
-            for(int j=0;j<i;j++){
-                ls+=nums[j];
-            }
-            for(int j=i+1;j<nums.length;j++){
-                rs+=nums[j];
-            }
-            if(ls==rs) return i;
+            if(arr[i]==arr[n]-arr[i+1]) return i;
         }
         return -1;
     }
