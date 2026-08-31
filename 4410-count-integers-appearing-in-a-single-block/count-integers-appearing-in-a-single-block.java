e63d04e0dc3679e3d6c0dd9989fe8709ed13707d
Apoorva Koche <apoorvakoche57@gmail.com>
2026-08-31 11:27:38 +0530

    Time: 2 ms (100.00%) | Memory: 43.5 MB (100.00%) - LeetSync

---
+++ 4410-count-integers-appearing-in-a-single-block/count-integers-appearing-in-a-single-block.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int countSpecialIntegers(int[] nums) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int i=0; i< nums.length;i++){
+            if(i==0 || nums[i] != nums[i-1]){
+                map.put(nums[i],map.getOrDefault(nums[i],0)+1);
+            }
+        }
+        int count=0;
+        for(Map.Entry<Integer,Integer> entry : map.entrySet()){
+            if(entry.getValue()==1){
+                count++;
+            }
+        }
+        return count;
+        
+    }
+}
